6dbd45ecf453811b4bbc0cb6c7943ea730e3fcd1
Viksit Arora <viksit@machine.(none)>
2014-02-22 14:23:42 +0530

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtisanMarket::Application.config.secret_key_base = 'e0fb8d7210280a7a3b162f15f2ce5a5792548355e315e249878853cf8afd6f3857247cddacb22cde4877306b78aac14b64810972a4499f0cf942ff1b365daef4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ArtisanMarket::Application.config.session_store :cookie_store, key: '_artisan_market_session'
